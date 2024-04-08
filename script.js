2e19a95fce1ba066e0d964803dcfb35848b5213c
AFarHitman7 <rafanhatim@gmail.com>
2024-04-08 12:52:21 +0530

    rafan commit almost done

--- script.js
+++ script.js
@@ -5,3 +5,33 @@ function toggleMenu(){
     icon.classList.toggle("open")
     
 }
+
+$(document).ready(function(){
+    $('#select_year').on('change', function() {
+    
+      //If red is selected, show red, hide yellow and blue.
+      if ( this.value == '0')
+      {
+        $("#0").show();
+        $("#1").hide();
+        $("#2").hide();
+      }
+      
+       //If yellow is selected, show yellow, hide red and blue.
+      if ( this.value == '1')
+      {
+        $("#0").hide();
+        $("#1").show();
+        $("#2").hide();
+      }
+      
+      //If blue is selected, show blue hide red and yellow.
+      if ( this.value == '2')
+      {
+        $("#0").hide();
+        $("#1").hide();
+        $("#2").show();
+      }
+      
+    });
+});
